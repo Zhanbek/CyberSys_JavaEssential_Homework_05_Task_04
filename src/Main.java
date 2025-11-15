3b9c95ef1643b340dfbe5f01288c143c901f3e74
Sarsenov <zhanbek.sarsenov@gmail.com>
2025-11-16 03:36:53 +0500

    Реалізує завдання

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,7 @@ public class Main {
     public static void main(String[] args) {
         List<Integer> myList = new ArrayList<>();
         fillList(myList);
+        System.out.println();
         System.out.println("Наш список містить наступні елементи:");
         System.out.println("--------------------------------");
         outputElements(myList);
